07240854ad8c1f37e868135bdfcfdafc0878f099
doreen <doreenwangeci38@gmail.com>
2024-02-15 13:52:00 +0300

    add edit&delete functionality&llocalStorage

--- src/app.js
+++ src/app.js
@@ -19,69 +19,123 @@ export default function createTodoApp() {
     modal.showModal();
   });
 
-  closeDialogeBtn.addEventListener('click', closeModal)
+  closeDialogeBtn.addEventListener('click', () => {
+    resetTask();
+    closeModal();
+  })
 
   closeDialogeIcon.addEventListener('click', closeModal)
 
   taskDetailForm.addEventListener('submit', dataValidation)
 
  //functions 
-  function closeModal() {
-    modal.close();
-}
-
-function dataValidation(e) {
-
-  e.preventDefault();
-
-  if(taskTitle.value === '') {
-    errorMsg.style.display = 'block';
-    errorMsg.textContent = 'Task can not be empty'
-  } else {
-    errorMsg.style.display = 'none';
-    errorMsg.textContent = '';
-    acceptData();
+    function closeModal() {
+      modal.close();
   }
-}
 
-let data = {};
-function acceptData() {
+  function dataValidation(e) {
 
- data['title'] = taskTitle.value;
- data['date'] = taskDate.value;
- data['details'] = taskDetails.value;
+    e.preventDefault();
 
- createTask();
+    if(taskTitle.value === '') {
+      errorMsg.textContent = 'Task can not be empty';
+      errorMsg.style.display = 'block';
+    } else {
+      errorMsg.style.display = 'none';
+      errorMsg.textContent = '';
+      acceptData();
+    }
+  }
 
- //IIFE to take close the modal after submitting
- (() => {
-  closeModal()
- })()
-}
+  //accept data and store it in local storage
+  let data = [];
+  function acceptData() {
 
-function createTask() {
-  tasks.innerHTML += ` <div class="task">
-    <h4>${data.title}</h4>
-    <p class="date">${data.date}</p>
-    <p class="details">${data.details}</p>
-    <span class="taskEditors">
-      <i onClick="deleteTask(this)" class="fa fa-trash" aria-hidden="true"></i>
-      <i onClick="editTask(this)" class="fa fa-pencil" aria-hidden="true"></i>
-    </span>
-  </div> `;
-  resetTask();
-}
+    const newTask = {
+      title: taskTitle.value,
+      date: taskDate.value,
+      details: taskDetails.value,
+    }
 
-//globally defined the delete and edit function, not the best practice
-window.deleteTask = function(e) {
-  e.parentElement.parentElement.remove();
-};
+    data.unshift(newTask);
 
-window.editTask = function(e) {
-  taskTitle.value = e.parentElement.previousSibling.innerHTML;
+    localStorage.setItem('data', JSON.stringify(data));
+
+    createTask();
+
+    //IIFE to take close the modal after submitting
+    (() => {
+      closeModal()
+    })()
+  }
+
+  function createTask() {
+    tasks.innerHTML = "";
+    data.map((selectedElement, index) => {
+      tasks.innerHTML += `<div class="task" 
+      id = ${index}>
+      <h4 class="h4">${selectedElement.title}</h4>
+      <p class="date">${selectedElement.date}</p>
+      <p class="details">${selectedElement.details}</p>
+      <span class="taskEditors">
+        <i onClick="deleteTask(this)" class="fa fa-trash" aria-hidden="true"></i>
+        <i onClick="editTask(this)" class="fa fa-pencil" aria-hidden="true"></i>
+      </span>
+    </div> `;
+    })
+
+    resetTask();
+  }
+
+  //globally defined the delete and edit function
+  window.deleteTask = function(e) {
+    e.parentElement.parentElement.remove();
+    data.splice(e.parentElement.parentElement.i, 1);
+    console.log(data);
+    localStorage.setItem('data', JSON.stringify(data));
+  };
+
+  function editTaskDialog(taskTitle, taskDate, taskDetails) {
+
+    document.getElementById('taskTitle').value = taskTitle;
+    document.getElementById('taskDate').value = taskDate;
+    document.getElementById('taskDetails').value = taskDetails;
+
+    // Open the dialog
+    modal.showModal();
+  }
+
+
+  window.editTask = function(e) {
+    const taskElement = e.parentElement.parentElement;
+    const taskTitle = taskElement.querySelector('.h4').textContent;
+    const taskDate = taskElement.querySelector('.date').textContent;
+    const taskDetails = taskElement.querySelector('.details').textContent;
+
+    editTaskDialog(taskTitle, taskDate, taskDetails);
+
+    // Remove the task from the list
+    taskElement.remove();
+  };
   
-};
-
+  window.onload = function() {
+      // Retrieve data from local storage
+      const taskArr = JSON.parse(localStorage.getItem('data')) || [];
+  
+      // Render tasks on the UI
+      taskArr.forEach(task => {
+          tasks.innerHTML += ` 
+              <div class="task">
+                  <h4 class="h4">${task.title}</h4>
+                  <p class="date">${task.date}</p>
+                  <p class="details">${task.details}</p>
+                  <span class="taskEditors">
+                      <i onclick="deleteTask(this)" class="fa fa-trash" aria-hidden="true"></i>
+                      <i onclick="editTask(this)" class="fa fa-pencil" aria-hidden="true"></i>
+                  </span>
+              </div> `;
+      });
+  };
 
 } 
 
@@ -89,6 +143,4 @@ const resetTask = () => {
   taskTitle.value = '';
   taskDate.value = '';
   taskDetails.value = '';
-
-
 }
